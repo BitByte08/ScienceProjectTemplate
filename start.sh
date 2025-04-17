346fdfba3ad78ee8217aad693f80c70e44ea6a5a
bitbyte08 <eight2823@naver.com>
2025-04-18 07:48:00 +0900

    fix :: start.sh 수정

--- start.sh
+++ start.sh
@@ -7,16 +7,17 @@ docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=qwer -e MYSQL_DATABASE=DB -p 3
 # broker
 docker build -t mqtt-broker ./broker
 docker rm -f mqtt-broker 2>/dev/null
-docker run -d --name mqtt-broker --network app-network -p 1883:1883 mqtt-broker
 
 # fastapi
 docker build -t mqtt-fastapi ./mqtt_fastapi
 docker rm -f mqtt-fastapi 2>/dev/null
-docker run -d --name mqtt-fastapi -e MYSQL_HOST=mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=qwer -e MYSQL_DATABASE=DB --network app-network -p 8000:8000 mqtt-fastapi
 
 # web (React)
 docker build -t mqtt-web ./web
 docker rm -f mqtt-web 2>/dev/null
 docker run -d --name mqtt-web --network app-network -p 3000:3000 mqtt-web
+docker run -d --name mqtt-broker --network app-network -p 1883:1883 mqtt-broker
+docker run -d --name mqtt-fastapi -e MYSQL_HOST=mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=qwer -e MYSQL_DATABASE=DB --network app-network -p 8000:8000 mqtt-fastapi
+
 
 echo "✅ All containers (broker, fastapi, web) are running!"
